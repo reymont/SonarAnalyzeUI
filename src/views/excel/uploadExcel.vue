8fa9b1190480975f6c86fe4de86d3ca2e42d1691
lijunsheng333 <907350373@qq.com>
2018-04-02 17:14:51 +0800

    init

---
+++ src/views/excel/uploadExcel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+    <!-- 上传excel文件 -->
+    <upload-excel-component @on-selected-file='selected'></upload-excel-component>
+    <!-- excel文件 - 列表 -->
+    <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
+      <el-table-column v-for='item of tableHeader' :prop="item" :label="item" :key='item'>
+      </el-table-column>
+    </el-table>
+
+  </div>
+</template>
+
+<script>
+import UploadExcelComponent from '@/components/UploadExcel/index.vue'
+
+export default {
+  name: 'uploadExcel',
+  components: { UploadExcelComponent },
+  data() {
+    return {
+      tableData: [],
+      tableHeader: []
+    }
+  },
+  methods: {
+    selected(data) {
+      this.tableData = data.results
+      this.tableHeader = data.header
+    }
+  }
+}
+</script>
